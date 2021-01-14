a05dfa480c7192234dfd47acbcc29069c5282210
Timothy Park <timothy_park@outlook.com>
2021-01-13 21:59:46 -0800

    Added Inflation

--- InvestmentCalculator.py
+++ InvestmentCalculator.py
@@ -1,7 +1,7 @@
 #Investment per year, percentage profits per year, years invested
-investment = 40000
-percentageIncrease = 50
-years = 5
+investment = 1000000
+percentageIncrease = 10
+years = 31
 
 def calcInvest(year):
     net = 0
@@ -11,8 +11,10 @@ def calcInvest(year):
         net = net*percent
     profit = net - (investment*year)
     gain = net/(investment*year)*100 -100
+    inflation = net*.98**year
     gain = "{:,.2f}%".format(gain)
     profit = "${:,.2f}".format(profit)
     net = "${:,.2f}".format(net)
-    return 'Net Worth: ' + net +'\nProfit: ' + profit + '\nGain: ' + gain
+    inflation = "${:,.2f}".format(inflation)
+    return 'Net Worth: ' + net +'\nProfit: ' + profit + '\nGain: ' + gain + '\nInflation: ' + inflation 
 print(calcInvest(years))
